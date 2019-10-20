b2d6d079b6c15f2959abfc972af7285b9c32da75
박재우 <wwkkii@naver.com>
2019-10-21 00:13:12 +0900

    충돌 체크
    칸에 맞게 클릭 했을때 동작

--- 2DGP/drag.py
+++ 2DGP/drag.py
@@ -4,137 +4,122 @@ import random
 
 os.chdir("D:\\Programming\\Project\\2DGP")
 
-class Locate:
-    def __init__(self):
-        self.location = False
-        self.dice = None
-
 
 class Dice:
-    def __init__(self):
-        global location
+    def __init__(self, index):
+        self.locate = False
         self.level = 1
+        self.index = index + 1
         self.image = load_image('image\\fire_dice.png')
         self.bullet = load_image('image\\fire_bullet.png')
         loop = True
         while loop:
-            self.location = random.randint(0, 15)
-            if self.location == 1 and location[0] == False:
+            if self.index == 1:
                 self.x = 290
                 self.y = 417
-                location[0] = True
                 loop = False
-            elif self.location == 2 and location[1] == False:
+            elif self.index == 2:
                 self.x = 363
                 self.y = 417
-                location[1] = True
                 loop = False
-            elif self.location == 3 and location[2] == False:
+            elif self.index == 3:
                 self.x = 436
                 self.y = 417
-                location[2] = True
                 loop = False
-            elif self.location == 4 and location[3] == False:
+            elif self.index == 4:
                 self.x = 509
                 self.y = 417
-                location[3] = True
                 loop = False
-            elif self.location == 5 and location[4] == False:
+            elif self.index == 5:
                 self.x = 290
                 self.y = 344
-                location[4] = True
                 loop = False
-            elif self.location == 6 and location[5] == False:
+            elif self.index == 6:
                 self.x = 363
                 self.y = 344
-                location[5] = True
                 loop = False
-            elif self.location == 7 and location[6] == False:
+            elif self.index == 7:
                 self.x = 436
                 self.y = 344
-                location[6] = True
                 loop = False
-            elif self.location == 8 and location[7] == False:
+            elif self.index == 8:
                 self.x = 509
                 self.y = 344
-                location[7] = True
                 loop = False
-            elif self.location == 9 and location[8] == False:
+            elif self.index == 9:
                 self.x = 290
                 self.y = 273
-                location[8] = True
                 loop = False
-            elif self.location == 10 and location[9] == False:
+            elif self.index == 10:
                 self.x = 363
                 self.y = 273
-                location[9] = True
                 loop = False
-            elif self.location == 11 and location[10] == False:
+            elif self.index == 11:
                 self.x = 436
                 self.y = 273
-                location[10] = True
                 loop = False
-            elif self.location == 12 and location[11] == False:
+            elif self.index == 12:
                 self.x = 509
                 self.y = 273
-                location[11] = True
                 loop = False
-            elif self.location == 13 and location[12] == False:
+            elif self.index == 13:
                 self.x = 290
                 self.y = 200
-                location[12] = True
                 loop = False
-            elif self.location == 14 and location[13] == False:
+            elif self.index == 14:
                 self.x = 363
                 self.y = 200
-                location[13] = True
                 loop = False
-            elif self.location == 15 and location[14] == False:
+            elif self.index == 15:
                 self.x = 436
                 self.y = 200
-                location[14] = True
                 loop = False
-            elif self.location == 16 and location[15] == False:
+            elif self.index == 16:
                 self.x = 509
                 self.y = 200
-                location[15] = True
                 loop = False
             else:
                 pass
 
+    def create(self):
+        self.locate = True
+
+    def delete(self):
+        self.locate = False
+
     def draw(self):
-        self.image.draw(self.x, self.y, 95, 95)
-        if self.level == 1:
-            self.bullet.draw(self.x + 2, self.y, 95, 95)
-        elif self.level == 2:
-            self.bullet.draw(self.x + 15 + 2, self.y + 15, 95, 95)
-            self.bullet.draw(self.x - 15 + 2, self.y - 15, 95, 95)
-        elif self.level == 3:
-            self.bullet.draw(self.x + 2, self.y, 95, 95)
-            self.bullet.draw(self.x + 15 + 2, self.y + 15, 95, 95)
-            self.bullet.draw(self.x - 15 + 2, self.y - 15, 95, 95)
-        elif self.level == 4:
-            self.bullet.draw(self.x + 15 + 2, self.y + 15, 95, 95)
-            self.bullet.draw(self.x - 15 + 2, self.y - 15, 95, 95)
-            self.bullet.draw(self.x + 15 + 2, self.y - 15, 95, 95)
-            self.bullet.draw(self.x - 15 + 2, self.y + 15, 95, 95)
-        elif self.level == 5:
-            self.bullet.draw(self.x + 2, self.y, 95, 95)
-            self.bullet.draw(self.x + 15 + 2, self.y + 15, 95, 95)
-            self.bullet.draw(self.x - 15 + 2, self.y - 15, 95, 95)
-            self.bullet.draw(self.x + 15 + 2, self.y - 15, 95, 95)
-            self.bullet.draw(self.x - 15 + 2, self.y + 15, 95, 95)
-        elif self.level == 6:
-            self.bullet.draw(self.x + 15 + 2, self.y + 15, 95, 95)
-            self.bullet.draw(self.x - 15 + 2, self.y - 15, 95, 95)
-            self.bullet.draw(self.x + 15 + 2, self.y - 15, 95, 95)
-            self.bullet.draw(self.x - 15 + 2, self.y + 15, 95, 95)
-            self.bullet.draw(self.x + 15 + 2, self.y, 95, 95)
-            self.bullet.draw(self.x - 15 + 2, self.y, 95, 95)
+        if self.locate:
+            self.image.draw(self.x, self.y, 95, 95)
+            if self.level == 1:
+                self.bullet.draw(self.x + 2, self.y, 95, 95)
+            elif self.level == 2:
+                self.bullet.draw(self.x + 15 + 2, self.y + 15, 95, 95)
+                self.bullet.draw(self.x - 15 + 2, self.y - 15, 95, 95)
+            elif self.level == 3:
+                self.bullet.draw(self.x + 2, self.y, 95, 95)
+                self.bullet.draw(self.x + 15 + 2, self.y + 15, 95, 95)
+                self.bullet.draw(self.x - 15 + 2, self.y - 15, 95, 95)
+            elif self.level == 4:
+                self.bullet.draw(self.x + 15 + 2, self.y + 15, 95, 95)
+                self.bullet.draw(self.x - 15 + 2, self.y - 15, 95, 95)
+                self.bullet.draw(self.x + 15 + 2, self.y - 15, 95, 95)
+                self.bullet.draw(self.x - 15 + 2, self.y + 15, 95, 95)
+            elif self.level == 5:
+                self.bullet.draw(self.x + 2, self.y, 95, 95)
+                self.bullet.draw(self.x + 15 + 2, self.y + 15, 95, 95)
+                self.bullet.draw(self.x - 15 + 2, self.y - 15, 95, 95)
+                self.bullet.draw(self.x + 15 + 2, self.y - 15, 95, 95)
+                self.bullet.draw(self.x - 15 + 2, self.y + 15, 95, 95)
+            elif self.level == 6:
+                self.bullet.draw(self.x + 15 + 2, self.y + 15, 95, 95)
+                self.bullet.draw(self.x - 15 + 2, self.y - 15, 95, 95)
+                self.bullet.draw(self.x + 15 + 2, self.y - 15, 95, 95)
+                self.bullet.draw(self.x - 15 + 2, self.y + 15, 95, 95)
+                self.bullet.draw(self.x + 15 + 2, self.y, 95, 95)
+                self.bullet.draw(self.x - 15 + 2, self.y, 95, 95)
 
 
 def handle():
-    global location
     global running
     events = get_events()
     for event in events:
@@ -142,31 +127,57 @@ def handle():
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-        elif event.type == SDL_MOUSEBUTTONDOWN and event.key == SDL_BUTTON_LEFT:
-            if location[0]:
-                pass
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            if 255 < event.x < 324 and 599 - 452 < event.y < 599 - 383:
+                dice[0].delete()
+            elif 363 - 35 < event.x < 363 + 34 and 599 - 417 - 35 < event.y < 599 - 417 + 34:
+                dice[1].delete()
+            elif 436 - 35 < event.x < 436 + 34 and 599 - 417 - 35 < event.y < 599 - 417 + 34:
+                dice[2].delete()
+            elif 509 - 35 < event.x < 509 + 34 and 599 - 417 - 35 < event.y < 599 - 417 + 34:
+                dice[3].delete()
+            elif 290 - 35 < event.x < 290 + 34 and 599 - 344 - 35 < event.y < 599 - 344 + 34:
+                dice[4].delete()
+            elif 363 - 35 < event.x < 363 + 34 and 599 - 344 - 35 < event.y < 599 - 344 + 34:
+                dice[5].delete()
+            elif 436 - 35 < event.x < 436 + 34 and 599 - 344 - 35 < event.y < 599 - 344 + 34:
+                dice[6].delete()
+            elif 509 - 35 < event.x < 509 + 34 and 599 - 344 - 35 < event.y < 599 - 344 + 34:
+                dice[7].delete()
+            elif 290 - 35 < event.x < 290 + 34 and 599 - 273 - 35 < event.y < 599 - 273 + 34:
+                dice[8].delete()
+            elif 363 - 35 < event.x < 363 + 34 and 599 - 273 - 35 < event.y < 599 - 273 + 34:
+                dice[9].delete()
+            elif 436 - 35 < event.x < 436 + 34 and 599 - 273 - 35 < event.y < 599 - 273 + 34:
+                dice[10].delete()
+            elif 509 - 35 < event.x < 509 + 34 and 599 - 273 - 35 < event.y < 599 - 273 + 34:
+                dice[11].delete()
+            elif 290 - 35 < event.x < 290 + 34 and 599 - 200 - 35 < event.y < 599 - 200 + 34:
+                dice[12].delete()
+            elif 363 - 35 < event.x < 363 + 34 and 599 - 200 - 35 < event.y < 599 - 200 + 34:
+                dice[13].delete()
+            elif 436 - 35 < event.x < 436 + 34 and 599 - 200 - 35 < event.y < 599 - 200 + 34:
+                dice[14].delete()
+            elif 509 - 35 < event.x < 509 + 34 and 599 - 200 - 35 < event.y < 599 - 200 + 34:
+                dice[15].delete()
 
 
-location = [False for i in range(16)]
+
+
 running = True
 
 open_canvas(800, 600)
 
 background = load_image("image\\background.png")
-dice_1 = Dice()
-dice_3 = Dice()
-dice_2 = Dice()
-dice_4 = Dice()
-dice_5 = Dice()
+dice = [Dice(i) for i in range(16)]
+for i in range(16):
+    dice[i].create()
 while running:
     handle()
     clear_canvas()
     background.draw(400, 300)
-    dice_1.draw()
-    dice_2.draw()
-    dice_3.draw()
-    dice_4.draw()
-    dice_5.draw()
+    for d in dice:
+        d.draw()
     update_canvas()
 
     delay(0.05)
