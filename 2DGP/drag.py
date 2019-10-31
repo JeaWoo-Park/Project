202a26c563196f0790689d5e336fcd63d187eb8e
박재우 <wwkkii@naver.com>
2019-10-31 20:10:11 +0900

    2D 2차 발표 자료

--- 2DGP/drag.py
+++ 2DGP/drag.py
@@ -202,157 +202,161 @@ def handle():
                 buy_button.mouse = True
             else:
                 buy_button.mouse = False
-        if event.type == SDL_MOUSEBUTTONUP and event.button == SDL_BUTTON_LEFT:
+        if event.type == SDL_MOUSEBUTTONUP:
+            print('button UP')
+            if event.button == SDL_BUTTON_LEFT:
+                if 255 < event.x < 324 and 599 - 452 < event.y < 599 - 383 and dice[0].locate:
+                    if dice[0].level == dice[index].level and index != 0:
+                        dice[index].off_drag()
+                        dice[0].level += 1
+                        dice[index].delete()
 
-            if 255 < event.x < 324 and 599 - 452 < event.y < 599 - 383 and dice[0].locate:
-                if dice[0].level == dice[index].level and index != 0:
-                    dice[index].off_drag()
-                    dice[0].level += 1
-                    dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
 
+                elif 363 - 35 < event.x < 363 + 34 and 599 - 417 - 35 < event.y < 599 - 417 + 34:
+
+                    if dice[1].level == dice[index].level and index != 1:
+                        dice[index].off_drag()
+                        dice[1].level += 1
+                        dice[index].delete()
+
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+
+                elif 436 - 35 < event.x < 436 + 34 and 599 - 417 - 35 < event.y < 599 - 417 + 34:
+
+                    if dice[2].level == dice[index].level and index != 2:
+                        dice[index].off_drag()
+                        dice[2].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+                elif 509 - 35 < event.x < 509 + 34 and 599 - 417 - 35 < event.y < 599 - 417 + 34:
+
+                    if dice[3].level == dice[index].level and index != 3:
+                        dice[index].off_drag()
+                        dice[3].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+                elif 290 - 35 < event.x < 290 + 34 and 599 - 344 - 35 < event.y < 599 - 344 + 34:
+                    if dice[4].level == dice[index].level and index != 4:
+                        dice[index].off_drag()
+                        dice[4].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+                elif 363 - 35 < event.x < 363 + 34 and 599 - 344 - 35 < event.y < 599 - 344 + 34:
+
+                    if dice[5].level == dice[index].level and index != 5:
+                        dice[index].off_drag()
+                        dice[5].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+                elif 436 - 35 < event.x < 436 + 34 and 599 - 344 - 35 < event.y < 599 - 344 + 34:
+
+                    if dice[6].level == dice[index].level and index != 6:
+                        dice[index].off_drag()
+                        dice[6].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+                elif 509 - 35 < event.x < 509 + 34 and 599 - 344 - 35 < event.y < 599 - 344 + 34:
+
+                    if dice[7].level == dice[index].level and index != 7:
+                        dice[index].off_drag()
+                        dice[7].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+                elif 290 - 35 < event.x < 290 + 34 and 599 - 273 - 35 < event.y < 599 - 273 + 34:
+
+                    if dice[8].level == dice[index].level and index != 8:
+                        dice[index].off_drag()
+                        dice[8].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+                elif 363 - 35 < event.x < 363 + 34 and 599 - 273 - 35 < event.y < 599 - 273 + 34:
+
+                    if dice[9].level == dice[index].level and index != 9:
+                        dice[index].off_drag()
+                        dice[9].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+                elif 436 - 35 < event.x < 436 + 34 and 599 - 273 - 35 < event.y < 599 - 273 + 34:
+
+                    if dice[10].level == dice[index].level and index != 10:
+                        dice[index].off_drag()
+                        dice[10].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+                elif 509 - 35 < event.x < 509 + 34 and 599 - 273 - 35 < event.y < 599 - 273 + 34:
+
+                    if dice[11].level == dice[index].level and index != 11:
+                        dice[index].off_drag()
+                        dice[11].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+                elif 290 - 35 < event.x < 290 + 34 and 599 - 200 - 35 < event.y < 599 - 200 + 34:
+
+                    if dice[12].level == dice[index].level and index != 12:
+                        dice[index].off_drag()
+                        dice[12].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+                elif 363 - 35 < event.x < 363 + 34 and 599 - 200 - 35 < event.y < 599 - 200 + 34:
+
+                    if dice[13].level == dice[index].level and index != 13:
+                        dice[index].off_drag()
+                        dice[13].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+                elif 436 - 35 < event.x < 436 + 34 and 599 - 200 - 35 < event.y < 599 - 200 + 34:
+
+                    if dice[14].level == dice[index].level and index != 14:
+                        dice[index].off_drag()
+                        dice[14].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
+                elif 509 - 35 < event.x < 509 + 34 and 599 - 200 - 35 < event.y < 599 - 200 + 34:
+
+                    if dice[15].level == dice[index].level and index != 15:
+                        dice[index].off_drag()
+                        dice[15].level += 1
+                        dice[index].delete()
+                    else:
+                        dice[index].off_drag()
+                        dice[index].go_back()
                 else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
+                    for index in range(16):
+                        dice[index].off_drag()
+                        dice[index].go_back()
 
-            elif 363 - 35 < event.x < 363 + 34 and 599 - 417 - 35 < event.y < 599 - 417 + 34:
 
-                if dice[1].level == dice[index].level and index != 1:
-                    dice[index].off_drag()
-                    dice[1].level += 1
-                    dice[index].delete()
-
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-
-            elif 436 - 35 < event.x < 436 + 34 and 599 - 417 - 35 < event.y < 599 - 417 + 34:
-
-                if dice[2].level == dice[index].level and index != 2:
-                    dice[index].off_drag()
-                    dice[2].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            elif 509 - 35 < event.x < 509 + 34 and 599 - 417 - 35 < event.y < 599 - 417 + 34:
-
-                if dice[3].level == dice[index].level and index != 3:
-                    dice[index].off_drag()
-                    dice[3].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            elif 290 - 35 < event.x < 290 + 34 and 599 - 344 - 35 < event.y < 599 - 344 + 34:
-                if dice[4].level == dice[index].level and index != 4:
-                    dice[index].off_drag()
-                    dice[4].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            elif 363 - 35 < event.x < 363 + 34 and 599 - 344 - 35 < event.y < 599 - 344 + 34:
-
-                if dice[5].level == dice[index].level and index != 5:
-                    dice[index].off_drag()
-                    dice[5].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            elif 436 - 35 < event.x < 436 + 34 and 599 - 344 - 35 < event.y < 599 - 344 + 34:
-
-                if dice[6].level == dice[index].level and index != 6:
-                    dice[index].off_drag()
-                    dice[6].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            elif 509 - 35 < event.x < 509 + 34 and 599 - 344 - 35 < event.y < 599 - 344 + 34:
-
-                if dice[7].level == dice[index].level and index != 7:
-                    dice[index].off_drag()
-                    dice[7].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            elif 290 - 35 < event.x < 290 + 34 and 599 - 273 - 35 < event.y < 599 - 273 + 34:
-
-                if dice[8].level == dice[index].level and index != 8:
-                    dice[index].off_drag()
-                    dice[8].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            elif 363 - 35 < event.x < 363 + 34 and 599 - 273 - 35 < event.y < 599 - 273 + 34:
-
-                if dice[9].level == dice[index].level and index != 9:
-                    dice[index].off_drag()
-                    dice[9].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            elif 436 - 35 < event.x < 436 + 34 and 599 - 273 - 35 < event.y < 599 - 273 + 34:
-
-                if dice[10].level == dice[index].level and index != 10:
-                    dice[index].off_drag()
-                    dice[10].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            elif 509 - 35 < event.x < 509 + 34 and 599 - 273 - 35 < event.y < 599 - 273 + 34:
-
-                if dice[11].level == dice[index].level and index != 11:
-                    dice[index].off_drag()
-                    dice[11].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            elif 290 - 35 < event.x < 290 + 34 and 599 - 200 - 35 < event.y < 599 - 200 + 34:
-
-                if dice[12].level == dice[index].level and index != 12:
-                    dice[index].off_drag()
-                    dice[12].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            elif 363 - 35 < event.x < 363 + 34 and 599 - 200 - 35 < event.y < 599 - 200 + 34:
-
-                if dice[13].level == dice[index].level and index != 13:
-                    dice[index].off_drag()
-                    dice[13].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            elif 436 - 35 < event.x < 436 + 34 and 599 - 200 - 35 < event.y < 599 - 200 + 34:
-
-                if dice[14].level == dice[index].level and index != 14:
-                    dice[index].off_drag()
-                    dice[14].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            elif 509 - 35 < event.x < 509 + 34 and 599 - 200 - 35 < event.y < 599 - 200 + 34:
-
-                if dice[15].level == dice[index].level and index != 15:
-                    dice[index].off_drag()
-                    dice[15].level += 1
-                    dice[index].delete()
-                else:
-                    dice[index].off_drag()
-                    dice[index].go_back()
-            else:
-                dice[index].off_drag()
-                dice[index].go_back()
         elif event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
@@ -509,7 +513,6 @@ while running:
 
     clear_canvas()
 
-    handle()
     background.draw(400, 300)
     buy_button.draw()
     for d in dice:
@@ -518,6 +521,8 @@ while running:
     for d in dice:
         if d.drag:
             d.draw()
+
+    handle()
     buy_button.update()
     for d in dice:
         d.update()
