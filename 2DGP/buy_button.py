fdbc65fe3686220605f54822043487d6cae6d4c2
박재우 <wwkkii@naver.com>
2019-11-01 10:56:22 +0900

    파일 분할

---
+++ 2DGP/buy_button.py
@@ -0,0 +1,18 @@
+from pico2d import *
+
+class Buy_Button:
+    def __init__(self):
+        self.image = load_image("image\\buy_button.png")
+        self.x = 400
+        self.y = 122
+        self.size = 79
+        self.mouse = False
+
+    def draw(self):
+        self.image.draw(self.x, self.y, self.size, self.size)
+
+    def update(self):
+        if self.mouse:
+            self.size = 82
+        else:
+            self.size = 79
