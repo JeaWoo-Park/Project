c059c92bd9e30f863a09e1b98aef58daf08b7640
박재우 <wwkkii@naver.com>
2019-11-28 17:16:36 +0900

    주사위 생성 비용 표시

--- 2DGP/main.py
+++ 2DGP/main.py
@@ -452,6 +452,7 @@ def draw():
     background.draw(400, 300)
     buy_button.draw()
     sp_font.draw(200, 105, '%d' % sp.point, (255, 255, 255))
+    sp_font.draw(400, 105, '%d' % sp.need_point, (255, 255, 255))
     for d in dice:
         if d.unit is not None:
             if not d.unit.drag:
